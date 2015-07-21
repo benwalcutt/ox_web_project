4bb34a0fd3bb447a63dcb523dac149404412967b
benwalcutt <bewalcut@email.uark.edu>
2015-07-20 19:18:27 -0500

    login functionality added

--- ox_web_project/ox_web/urls.py
+++ ox_web_project/ox_web/urls.py
@@ -4,4 +4,6 @@ from ox_web import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^about/$', views.about, name='about'),
+	url(r'^login/$', views.user_login, name='login'),
+	url(r'^logout/$', views.user_logout, name='logout'),
 )
--- ox_web_project/ox_web/views.py
+++ ox_web_project/ox_web/views.py
@@ -1,7 +1,36 @@
+from ox_web.forms import UserForm, UserProfileForm
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+@login_required
+def user_logout(request):
+  logout(request)
+  return HttpResponseRedirect('/ox_web/')
+
+def user_login(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
+    
+    if user:
+      if user.is_active:
+        login(request, user)
+        return HttpResponseRedirect('/ox_web/')
+      else:
+        return HttpResponse('Your account has been disabled. <a href="/ox_web/">Home</a>')
+
+    else:
+      print "Invalid login details: {0), {1)".format(username, password)
+      return HttpResponse('Invalid login details supplied. <a href="/ox_web/">Home</a>')
+
+  else:
+    return render(request, 'ox_web/login.html', {})
 
 def index(request):
   context_dict = {}
